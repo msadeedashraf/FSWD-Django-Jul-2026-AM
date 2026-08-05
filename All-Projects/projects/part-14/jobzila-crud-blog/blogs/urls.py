32fee0cad19eb6aefdd79bdacbd8dd8cc43b3f74
Sadeed <msadeedashraf@gmail.com>
2026-08-05 12:17:55 -0400

    .

--- All-Projects/projects/part-14/jobzila-crud-blog/blogs/urls.py
+++ All-Projects/projects/part-14/jobzila-crud-blog/blogs/urls.py
@@ -9,8 +9,15 @@ urlpatterns = [
     path("<int:blog_id>/", views.blog_detail, name="blog_detail"),
     
     
-    # path("<int:blog_id>/edit/", views.edit_blog,name="edit_blog",),
+     path(
+        "<int:blog_id>/edit/",
+        views.edit_blog,
+        name="edit_blog",
+    ),
 
-    # path("<int:blog_id>/delete/",views.delete_blog,name="delete_blog",),
-    
+    path(
+        "<int:blog_id>/delete/",
+        views.delete_blog,
+        name="delete_blog",
+    ),
 ]
--- All-Projects/projects/part-14/jobzila-crud-blog/blogs/views.py
+++ All-Projects/projects/part-14/jobzila-crud-blog/blogs/views.py
@@ -1,11 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404, redirect, render
-from django.shortcuts import render, redirect
 
 from .forms import BlogForm
-
 from .models import Blog
 
 # Create your views here.
@@ -20,7 +17,14 @@ def blog(request):
         blogs = blogs.filter(title__icontains=quick_search)
 
 
-    return render(request, "blogs/blogs.html", {"blogs": blogs})
+    return render(
+    request,
+    "blogs/blogs.html",
+    {
+        "blogs": blogs,
+        "quick_search": quick_search,
+    },
+)
 
 @login_required(login_url="/users/login/")
 def add_blog(request):
@@ -63,5 +67,67 @@ def blog_detail(request, blog_id):
         "blogs/blog_detail.html",
         {
             "blog": selected_blog,
+        },
+    )
+
+
+@login_required(login_url="/users/login/")
+def edit_blog(request, blog_id):
+    blog = get_object_or_404(Blog, id=blog_id)
+
+    # Blog.author is currently a text field containing the username.
+    if blog.author != request.user.username:
+        raise PermissionDenied
+
+    if request.method == "POST":
+        form = BlogForm(
+            request.POST,
+            request.FILES,
+            instance=blog,
+        )
+
+        if form.is_valid():
+            updated_blog = form.save(commit=False)
+
+            # Prevent someone from changing the author.
+            updated_blog.author = request.user.username
+            updated_blog.save()
+
+            return redirect(
+                "blogs:blog_detail",
+                blog_id=blog.id,
+            )
+
+    else:
+        form = BlogForm(instance=blog)
+
+    return render(
+        request,
+        "blogs/edit_blog.html",
+        {
+            "form": form,
+            "blog": blog,
+        },
+    )
+
+
+@login_required(login_url="/users/login/")
+def delete_blog(request, blog_id):
+    blog = get_object_or_404(Blog, id=blog_id)
+
+    # Only the blog author can delete the blog.
+    if blog.author != request.user.username:
+        raise PermissionDenied
+
+    if request.method == "POST":
+        blog.delete()
+
+        return redirect("blogs:blog")
+
+    return render(
+        request,
+        "blogs/delete_blog.html",
+        {
+            "blog": blog,
         },
     )
